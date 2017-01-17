cae7dce013af2d91b63e4aaa30750c4050ac0af4
Thomas Merkel <tm@core.io>
2017-01-17 13:35:52 +0100

    Update to newest core-base and pgsql database version

--- copy/var/zoneinit/includes/23-pgsql.sh
+++ copy/var/zoneinit/includes/23-pgsql.sh
@@ -1,2 +1,14 @@
+#!/usr/bin/env bash
+# Force locale to UTF-8
+LC_ALL=en_US.UTF-8
+
+# Create `data` folder for the pgsql cluster
+PGDATA='/var/pgsql/data'
+mkdir -p ${PGDATA}
+chown postgres:postgres ${PGDATA}
+
+# initdb -- create a new PostgreSQL database cluster
+su postgres -c 'initdb -D /var/pgsql/data/'
+
 # TODO: create user + db + ...
-/usr/sbin/svcadm enable svc:/pkgsrc/postgresql:default
+svcadm enable svc:/pkgsrc/postgresql:default
