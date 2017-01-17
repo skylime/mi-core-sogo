8e3b98cbda9ca56ff0fdf7cb51b9cc2bb4f5f7c7
Thomas Merkel <tm@core.io>
2017-01-17 15:16:37 +0100

    Fix imapproxy default options

--- copy/var/zoneinit/includes/24-imapproxy.sh
+++ copy/var/zoneinit/includes/24-imapproxy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # Configure imapproxy and enable the service if possible
 
-if mdata-get imap_host 1>/dev/null 2>&1; then
-	imap_host=$(mdata-get imap_host)
-	gsed -i 's:__SERVER_HOSTNAME__:${imap_host}:g' /opt/local/etc/imapproxy.conf
+if mdata-get imap_server 1>/dev/null 2>&1; then
+	imap_server=$(mdata-get imap_server)
+	gsed -i "s:__SERVER_HOSTNAME__:${imap_server}:g" /opt/local/etc/imapproxy.conf
 	svcadm enable svc:/network/imapproxy:default
 fi
