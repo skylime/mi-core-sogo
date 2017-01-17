1b1b908467db9ae1b4480ae08af2ac71e7f2a42a
Thomas Merkel <tm@core.io>
2017-01-17 13:47:22 +0100

    Create sogo pgsql user and database

--- copy/var/zoneinit/includes/23-pgsql.sh
+++ copy/var/zoneinit/includes/23-pgsql.sh
@@ -10,5 +10,14 @@ chown postgres:postgres ${PGDATA}
 # initdb -- create a new PostgreSQL database cluster
 su postgres -c 'initdb -D /var/pgsql/data/'
 
-# TODO: create user + db + ...
+# Configure SOGo PGSQL user and database
+SOGO_PGSQL_DB='sogo'
+SOGO_PGSQL_PW=${SOGO_PGSQL_PW:-$(mdata-get sogo_pgsql_pw 2>/dev/null)} || \
+	SOGO_PGSQL_PW=$(od -An -N8 -x /dev/random | head -1 | tr -d ' ');
+mdata-put sogo_pgsql_pw "${SOGO_PGSQL_PW}"
+
+sm-create-db postgresql ${SOGO_PGSQL_DB}
+sm-create-dbuser postgresql ${SOGO_PGSQL_DB} ${SOGO_PGSQL_PW} ${SOGO_PGSQL_DB}
+
+# Enable pgsql service
 svcadm enable svc:/pkgsrc/postgresql:default
