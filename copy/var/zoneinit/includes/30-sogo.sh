a113c8ef8ed39f6ec6ce5c1f336c44c6d1469858
Thomas Merkel <tm@core.io>
2017-01-18 13:45:24 +0100

    Add cron and database for kumquat user import

--- copy/var/zoneinit/includes/30-sogo.sh
+++ copy/var/zoneinit/includes/30-sogo.sh
@@ -28,5 +28,24 @@ gsed -i \
 	-e "s/__SOGO_SUPERUSER__/${SOGO_SUPERUSER}/" \
 	/opt/local/etc/sogo/sogo.conf
 
+# Install sogo_users database and cronjob for user import
+psql -U ${SOGO_PGSQL_USER} -X --set ON_ERROR_STOP=on ${SOGO_PGSQL_DB} <<__SQL__
+CREATE TABLE IF NOT EXISTS sogo_users
+(
+  c_uid character varying(512) NOT NULL,
+  c_name character varying(512),
+  c_password character varying(256),
+  c_cn character varying(512),
+  mail character varying(512),
+  CONSTRAINT sogo_users_pkey PRIMARY KEY (c_uid)
+)
+WITH (
+  OIDS=FALSE
+);
+__SQL__
+
+CRON="0,5,10,15,20,25,30,35,40,45,55 * * * * su sogo -c '/opt/core/bin/import-cron'"
+(crontab -l 2>/dev/null || true; echo "$CRON" ) | sort | uniq | crontab
+
 # Enable sogo service
 svcadm enable svc:/www/sogo:default
