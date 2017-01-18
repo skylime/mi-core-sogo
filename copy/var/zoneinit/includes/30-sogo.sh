94bc0e63e57b8432f06ae3a1f52795597826d20d
Thomas Merkel <tm@core.io>
2017-01-18 09:54:04 +0100

    Add script to configure sogo with mdata info

--- copy/var/zoneinit/includes/30-sogo.sh
+++ copy/var/zoneinit/includes/30-sogo.sh
@@ -2,6 +2,11 @@
 # Search and replace in the default SOGo template to connect to database
 # and setup title and all information provided by mdata
 
+# Receive mdata information for sieve, title and superuser
+SIEVE_SERVER=$(mdata-get imap_server 2>/dev/null)
+SOGO_TITLE=$(mdata-get sogo_title 2>/dev/null)
+SOGO_SUPERUSER=$(mdata-get sogo_superuser 2>/dev/null)
+
 # Configure SOGo PGSQL user and database
 SOGO_PGSQL_DB='sogo'
 SOGO_PGSQL_USER='sogo'
@@ -13,3 +18,12 @@ mdata-put sogo_pgsql_pw "${SOGO_PGSQL_PW}"
 sm-create-db postgresql ${SOGO_PGSQL_DB}
 sm-create-dbuser postgresql ${SOGO_PGSQL_USER} ${SOGO_PGSQL_PW} ${SOGO_PGSQL_DB}
 
+# Search and replace configuration template
+gsed -i \
+	-e "s/__SOGO_PGSQL_USER__/${SOGO_PGSQL_USER}/" \
+	-e "s/__SOGO_PGSQL_PW__/${SOGO_PGSQL_USER}/" \
+	-e "s/__SOGO_PGSQL_DB__/${SOGO_PGSQL_DB}/" \
+	-e "s/__SIEVE_SERVER__/${SIEVE_SERVER}/" \
+	-e "s/__SOGO_TITLE__/${SOGO_TITLE}/" \
+	-e "s/__SOGO_SUPERUSER__/${SOGO_SUPERUSER}/"
+	/opt/local/etc/sogo/sogo.conf
