6d6a4c8f1d09a76932cc37e4b303852058a338cb
Thomas Merkel <tm@core.io>
2017-01-18 13:46:57 +0100

    create data only if not exists

--- copy/var/zoneinit/includes/23-pgsql.sh
+++ copy/var/zoneinit/includes/23-pgsql.sh
@@ -2,13 +2,17 @@
 # Force locale to UTF-8
 LC_ALL=en_US.UTF-8
 
-# Create `data` folder for the pgsql cluster
 PGDATA='/var/pgsql/data'
-mkdir -p ${PGDATA}
-chown postgres:postgres ${PGDATA}
 
-# initdb -- create a new PostgreSQL database cluster
-su postgres -c 'initdb -D /var/pgsql/data/'
+# Create only if not exists
+if [ ! -d ${PGDATA} ]; then
+	# Create `data` folder for the pgsql cluster
+	mkdir -p ${PGDATA}
+	chown postgres:postgres ${PGDATA}
+
+	# initdb -- create a new PostgreSQL database cluster
+	su postgres -c 'initdb -D /var/pgsql/data/'
+fi
 
 # Enable pgsql service
 svcadm enable svc:/pkgsrc/postgresql:default
