229f01edbe5c2df411d27f07e9e93a40d5a897b3
Thomas Merkel <tm@core.io>
2017-01-19 13:18:57 +0100

    Allow user search in groupware sogo

--- copy/var/zoneinit/includes/29-sogo-groupware.sh
+++ copy/var/zoneinit/includes/29-sogo-groupware.sh
@@ -8,4 +8,8 @@ if mdata-get sogo_groupware 1>/dev/null 2>&1; then
 		-e "s/SOGoExternalAvatarsEnabled = NO;/SOGoExternalAvatarsEnabled = YES;/" \
 		-e "s/SharedModification.js/GroupwareModification.js/" \
 		/opt/local/etc/sogo/sogo.conf
+
+	gsed -i \
+		-e "s/\(rewrite.*usersSearch @404 last\)/#\1/" \
+		/opt/local/etc/nginx/nginx.conf
 fi
