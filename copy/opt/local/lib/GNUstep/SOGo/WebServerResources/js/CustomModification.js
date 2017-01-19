efb58791720fd70f578581b4e555c42f0397db04
Thomas Merkel <tm@core.io>
2017-01-19 12:03:00 +0100

    Hide search and delegate options

--- copy/opt/local/lib/GNUstep/SOGo/WebServerResources/js/CustomModification.js
+++ copy/opt/local/lib/GNUstep/SOGo/WebServerResources/js/CustomModification.js
@@ -3,9 +3,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
   // hide email right/sharing menu entries (as they do not work with some imap servers)
   var hideElements = [
-    'body > main > md-sidenav > md-content > section > md-list > md-list-item > div > div.md-secondary-container > button:nth-child(1)',
+    'body > main > md-sidenav > md-content > section > md-list > md-list-item > div > div.md-secondary-container > button:nth-child(2)',
     '.md-open-menu-container > md-menu-content > md-menu-item[ng-show*=additional]',
     '.md-open-menu-container > md-menu-content > md-menu-divider[ng-show*=additional]',
+    '.md-open-menu-container > md-menu-content > md-menu-item[ng-show*="account.id"]',
+    'md-dialog > md-dialog-content > div > md-autocomplete[md-selected-item-change="acl.addUser(user)"]',
+    'md-dialog > md-dialog-content > div > md-icon',
   ];
 
   var css = hideElements.join(', ') + ' { display: none !important; }\n';
