773da650371f7da620d1fc41f707429b5868dcca
Thomas Merkel <tm@core.io>
2017-01-19 13:14:41 +0100

    Enable 'groupware' metadata information
    This will enable some SOGo options and use different JS scripts

---
+++ copy/opt/local/lib/GNUstep/SOGo/WebServerResources/js/GroupwareModification.js
@@ -0,0 +1,28 @@
+// custom mods
+document.addEventListener("DOMContentLoaded", function(event) {
+
+  // hide email right/sharing menu entries (as they do not work with some imap servers)
+  var hideElements = [
+    'body > main > md-sidenav > md-content > section > md-list > md-list-item > div > div.md-secondary-container > button:nth-child(2)',
+    '.md-open-menu-container > md-menu-content > md-menu-item[ng-show*=additional]',
+    '.md-open-menu-container > md-menu-content > md-menu-divider[ng-show*=additional]',
+  ];
+
+  var css = hideElements.join(', ') + ' { display: none !important; }\n';
+ 
+  css += 'input[ng-model="$AccountDialogController.account.identities[0].email"] { pointer-events: none; tabindex: -1; color: rgba(0,0,0,0.38);}'
+ 
+  // insert styles
+  var head = document.head || document.getElementsByTagName('head')[0];
+  var style = document.createElement('style');
+  
+  style.type = 'text/css';
+  if (style.styleSheet){
+    style.styleSheet.cssText = css;
+  } else {
+    style.appendChild(document.createTextNode(css));
+  }
+
+  head.appendChild(style);
+
+});
--- copy/var/zoneinit/includes/29-sogo-groupware.sh
+++ copy/var/zoneinit/includes/29-sogo-groupware.sh
@@ -6,5 +6,6 @@ if mdata-get sogo_groupware 1>/dev/null 2>&1; then
 	gsed -i \
 		-e "s/isAddressBook = NO;/isAddressBook = YES;/" \
 		-e "s/SOGoExternalAvatarsEnabled = NO;/SOGoExternalAvatarsEnabled = YES;/" \
+		-e "s/SharedModification.js/GroupwareModification.js/" \
 		/opt/local/etc/sogo/sogo.conf
 fi
