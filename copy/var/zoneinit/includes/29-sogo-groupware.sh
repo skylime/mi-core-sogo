e524632f1509b9094096420e2eb8d037f872b1b9
Thomas Merkel <tm@core.io>
2017-01-19 13:10:54 +0100

    enable sogo groupware features via gsed script in sogo.conf

---
+++ copy/var/zoneinit/includes/29-sogo-groupware.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# Allow the user to control the groupware feature
+
+if mdata-get sogo_groupware 1>/dev/null 2>&1; then
+	# Require magic gsed to modify some defaults
+	gsed -i \
+		-e "s/isAddressBook = NO;/isAddressBook = YES;/" \
+		-e "s/SOGoExternalAvatarsEnabled = NO;/SOGoExternalAvatarsEnabled = YES;/" \
+		/opt/local/etc/sogo/sogo.conf
+fi
