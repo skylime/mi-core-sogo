b25694c28394ce15101306cffe8578b17ec55c87
Thomas Merkel <tm@core.io>
2017-01-25 16:50:34 +0100

    Add and fix lets encrypt renew process

--- copy/var/zoneinit/includes/45-ssl.sh
+++ copy/var/zoneinit/includes/45-ssl.sh
@@ -21,10 +21,14 @@ if mdata-get sogo_ssl 1>/dev/null 2>&1; then
 else
 	# Try to generate let's encrypt ssl certificate for the hostname
 	if /opt/core/bin/ssl-letsencrypt.sh 1>/dev/null; then
-		LE_LIVE="/opt/local/etc/letsencrypt/live/$(hostname)/"
+		LE_HOME='/opt/local/etc/letsencrypt/'
+		LE_LIVE="${LE_HOME}live/$(hostname)/"
 		# Workaround to link correct files for SSL_HOME
 		ln -s ${LE_LIVE}/fullchain.pem ${SSL_HOME}/nginx.crt
 		ln -s ${LE_LIVE}/privkey.pem ${SSL_HOME}/nginx.key
+		# Update renew-hook.sh
+		echo '#!/usr/bin/env bash' > ${LE_HOME}renew-hook.sh
+		echo 'svcadm restart svc:/pkgsrc/nginx:default' >> ${LE_HOME}renew-hook.sh
 	else
 		# Fallback to selfsigned ssl certificates
 		/opt/core/bin/ssl-selfsigned.sh -d ${SSL_HOME} -f nginx
